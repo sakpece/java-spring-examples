ff6e089ec19fc27c237505204dfbf32c2ce80c04
sakpece <sakpece@gmail.com>
2020-01-18 16:54:10 +0530

    spring boot actuator application first example
    spring boot actuator application first example

---
+++ spring-boot-actuator-example/src/main/java/com/adaabindia/DemoRestController.java
@@ -0,0 +1,12 @@
+package com.adaabindia;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DemoRestController 
+{
+@GetMapping("/hello")
+public String hello() 
+{
+return "Hello User!";
+}
+}
